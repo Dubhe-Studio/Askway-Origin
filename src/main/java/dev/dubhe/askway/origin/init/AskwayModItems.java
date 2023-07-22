28ee6189f1ca59e254ad62997b94583bd8ec9629
DancingSnow <1121149616@qq.com>
2023-07-22 23:54:44 +0800

    添加两种桃木剑

--- src/main/java/dev/dubhe/askway/origin/init/AskwayModItems.java
+++ src/main/java/dev/dubhe/askway/origin/init/AskwayModItems.java
@@ -1,8 +1,14 @@
 package dev.dubhe.askway.origin.init;
 
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import dev.dubhe.askway.origin.items.MagicTestItem;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import org.jetbrains.annotations.NotNull;
 
 import static dev.dubhe.askway.origin.AskwayOrigin.REGISTRATE;
@@ -18,6 +24,32 @@ public class AskwayModItems {
 
     public static final RegistryEntry<MagicTestItem> MAGIC_TEST = REGISTRATE
             .item("magic_test", MagicTestItem::new)
+            .register();
+
+    public static final RegistryEntry<SwordItem> PEACH_WOODEN_SWORD = REGISTRATE
+            .item("peach_wooden_sword", p -> new SwordItem(Tiers.WOOD, 3, -2.4F, p))
+            .properties(p -> p.stacksTo(1).durability(56).defaultDurability(56))
+            .recipe((ctx, provider) -> ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ctx.get())
+                    .pattern("A")
+                    .pattern("A")
+                    .pattern("B")
+                    .define('A', AskwayModBlocks.PEACH_PLANKS.get())
+                    .define('B', Items.STICK)
+                    .unlockedBy("hasitem", RegistrateRecipeProvider.has(AskwayModBlocks.PEACH_PLANKS.get()))
+                    .save(provider))
+            .register();
+
+    public static final RegistryEntry<SwordItem> LIGHTNING_PEACH_WOODEN_SWORD = REGISTRATE
+            .item("lightning_peach_wooden_sword", p -> new SwordItem(Tiers.WOOD, 3, -2.4F, p))
+            .properties(p -> p.stacksTo(1).durability(56).defaultDurability(56))
+            .recipe((ctx, provider) -> ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ctx.get())
+                    .pattern("A")
+                    .pattern("A")
+                    .pattern("B")
+                    .define('A', AskwayModBlocks.LIGHTNING_PEACH_PLANKS.get())
+                    .define('B', Items.STICK)
+                    .unlockedBy("hasitem", RegistrateRecipeProvider.has(AskwayModBlocks.LIGHTNING_PEACH_PLANKS.get()))
+                    .save(provider))
             .register();
 
 
